37feab9be56066e37f7da8bc717d9507074c7561
Elie <e.na.hatem@gmail.com>
2021-07-25 22:29:54 +0200

    Optimization prolem working

---
+++ main.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.numeric import ones_like
+
+from scipy.optimize import minimize
+from scipy.optimize import Bounds
+from scipy.optimize import NonlinearConstraint
+
+
+from scipy import integrate
+
+from nonlinear_bounds import nl_con
+from objective_function import obj
+
+from utils import *
+
+# Drone parameters
+m =  29.5e-3 / 2 #  mass
+Ixx =  1.657171e-05 # Inertia
+l = 0.046 # arm length
+
+# Constants
+t_step = 0.01 # 100 Hz
+g = 9.81 # m/s^2
+
+# Bounds on the z trajectory of the reaching phase
+z1_min = 0.8
+z1_max = 3
+
+# Bounds on the z trajectory of the flipping phase
+z2_min = 0.8
+z2_max = 3
+
+# Bounds on the z trajectory of the recovery phase
+z3_min = 0.8
+z3_max = 3
+
+# Maximum thrust and torque reachable by the drone
+u1_max = 0.9 * ( ( 46e-3 * g ) / 2 ) # Maximum thrust 
+u2_max = 0.1 * ( 1 / 2 * u1_max * l) # Maximum torque
+
+## Constraints applied on the optimization problem
+
+# lower bound on the initial phi angle at the start of the reaching phase
+phi_start_min = 0
+
+# upper bound on the final phi angle at the end of the reaching phase
+phi_start_max = np.pi/2
+
+# lower bound on the initial phi angle at the end of the flipping phase (start of the recovery phase)
+phi_end_min = 3/2*np.pi
+
+# upper bound on the final phi angle at the end of the recovery phase
+phi_end_max   = 2*np.pi
+
+# bounds on t1 (time of the reaching phase trajectory)
+t1_min = 0.1
+t1_max = np.inf
+
+# bounds on t2 (time of the flipping phase trajectory)
+t2_min = 0.1
+t2_max = np.inf
+
+# bounds on t3 (time of the recovery phase trajectory)
+t3_min = 0.1
+t3_max = np.inf
+
+# setting the lower and upper bounds for each vaiable
+
+bnd_z1          = (z1_min, z1_max)
+bnd_z2          = (z1_min, z1_max)
+bnd_z3          = (z2_min, z2_max)
+bnd_z4          = (z3_min, z3_max)
+bnd_phi_start   = (phi_start_min, phi_start_max)
+bnd_phi_end     = (phi_end_min, phi_end_max)
+bnd_t1          = (t1_min, t1_max)
+bnd_t2          = (t2_min, t2_max)
+bnd_t3          = (t3_min, t3_max)
+
+bnds = (bnd_z1, bnd_z2, bnd_z3, bnd_z4,
+        bnd_phi_start, bnd_phi_end, 
+        bnd_t1, bnd_t2, bnd_t3)
+
+# bounds = Bounds (   [z1_min, z1_max],               # z1 bounds 
+#                     [z1_min, z1_max],               # z2 bounds
+#                     [z2_min, z2_max],               # z3 bounds
+#                     [z3_min, z3_max],              # z4 bounds
+#                     [phi_start_min, phi_start_max], # phi_start bounds
+#                     [phi_end_min, phi_end_max],     # phi_end bounds
+#                     [t1_min, t1_max],               # t1 bounds
+#                     [t2_min, t2_max],               # t2 bounds
+#                     [t3_min, t3_max])               # t3 bounds
+
+nonlinear_constraint = NonlinearConstraint(nl_con, -np.inf, 0)
+
+x0 = np.array([ 0.9, 1.9, 1.9, 0.9, np.pi/2-0.2, (3/2+0.2)*np.pi, 0.4, 0.3, 0.4 ])
+
+sol = minimize(obj, x0, method = 'trust-constr',
+                constraints=nonlinear_constraint,
+                options={'verbose': 1}, bounds=bnds)
---
+++ nonlinear_bounds.py
@@ -0,0 +1,124 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.core.numeric import ones_like
+
+from scipy.optimize import minimize
+from scipy.optimize import NonlinearConstraint
+from scipy import integrate
+from utils import *
+
+def nl_con(x):
+
+    # Drone parameters
+    m =  29.5e-3 / 2 #  mass
+    Ixx =  1.657171e-05 # Inertia
+    l = 0.046 # arm length
+
+    # Constants
+    g = 9.81 
+    t_step = 0.01
+    
+    # Control bounds
+    # Maximum thrust and torque reachable by the drone
+    u1_max = 0.9 * ( ( 46e-3 * g ) / 2 ) # Maximum thrust 
+    u1_min = 0
+    u2_max = 0.1 * ( 1 / 2 * u1_max * l) # Maximum torque
+    u2_min = -u2_max
+
+    z1 = x[0]  # initial height of the reaching phase
+    z2 = x[1]  # initial height of the flipping phase
+    z3 = x[2]  # final   height of the flipping phase
+    z4 = x[3]  # final   height of the recovery phase
+
+    phi_start = x[4] # initial roll angle of the flipping phase
+    phi_end   = x[5] # final roll angle of the flipping phase
+
+    t1 = x[6]
+    t2 = x[7]
+    t3 = x[8]
+
+    ## Trajectory waypoints
+    # For z
+    Z1 = np.array([z1, 0, 0, 0, 0]) # initial position (start of the reaching phase)
+    Z2 = np.array([z2, ((z3-z2)/t2 + g*t2/2), -g, 0, 0])
+    Z3 = np.array([z3, ((z3-z2)/t2 - g*t2/2), -g, 0, 0])
+    Z4 = np.array([z4, 0, 0, 0, 0])
+
+    # For phi
+    PHI1 = np.array([0, 0, 0, 0, 0])
+    PHI2 = np.array([phi_start, (phi_end - phi_start)/t2, 0, 0, 0])
+    PHI3 = np.array([phi_end, (phi_end - phi_start)/t2, 0, 0, 0])
+    PHI4 = np.array([2*np.pi, 0, 0, 0, 0])
+
+    ## Reaching phase
+    # For Z
+    traj1_z = trajectory(Z1,Z2,t1,t_step)
+
+    # For phi
+    traj1_phi = trajectory(PHI1,PHI2,t1,t_step)
+    T1 = np.arange(0,t1,t_step)
+
+    ## Flip phase
+    # For z
+
+    coeff_z2   = np.array([-g/2, ((z3-z2)/t2+g*t2/2), z2])
+    coeff_zd2  = np.polyder(coeff_z2)
+    coeff_zdd2 = np.polyder(coeff_zd2)
+
+    traj_z2    = np.polyval(coeff_z2, np.arange(0,t2,t_step))
+    traj_zd2   = np.polyval(coeff_zd2, np.arange(0,t2,t_step)) 
+    traj_zdd2  = np.polyval(coeff_zdd2,np.arange(0,t2,t_step))
+
+    traj2_z = np.array([
+            traj_z2,
+            traj_zd2,
+            traj_zdd2])
+
+    # For phi
+    coeff_phi2 = np.array([(phi_end-phi_start)/t2, phi_start])
+    coeff_phid2 = np.polyder(coeff_phi2)
+    coeff_phidd2 = np.polyder(coeff_phid2)
+    phi2 = np.polyval(coeff_phi2, np.arange(0,t2,t_step))
+    phid2 = np.polyval(coeff_phid2, np.arange(0,t2,t_step))
+    phidd2 = np.polyval(coeff_phidd2,np.arange(0,t2,t_step))
+    traj2_phi =np.array([
+                phi2,
+                phid2,
+                phidd2])
+
+    T2 = np.arange(t1,t1+t2, t_step)
+
+    ## Recovery phase
+    # For z
+    traj3_z = trajectory(Z3,Z4,t3,t_step)
+
+    # For phi
+    traj3_phi = trajectory(PHI3,PHI4,t3,t_step)
+
+    T3 = np.arange(t1+t2,t1+t2+t3,t_step)
+
+    t_tot = np.arange(0,t1+t2+t3,t_step)
+
+    z = np.hstack([ traj1_z[0,:], traj_z2, traj3_z[0,:] ])
+    phi = np.hstack([ traj1_phi[0,:], phi2, traj3_phi[0,:] ])
+
+    ## Calculating the thrust u1 along the trajectory
+    zdd = np.hstack([ traj1_z[2,:], traj_zdd2, traj3_z[2,:] ])
+    phidd = np.hstack([ traj1_phi[2,:], phidd2, traj3_phi[2,:] ])
+
+    gravity = g * ones_like(z)
+
+    u1 = m * (zdd + gravity) / np.cos(phi)
+    u2 = phidd * Ixx
+
+    return np.array([
+        z1-z2,
+        z3-z2,
+        z4-z3,
+        -np.min(u1),
+        np.max(u1)-u1_max,
+        u2_min - np.min(u2),
+        np.max(u2)-u2_max
+    ])
+    
+    # return NonlinearConstraint(A_ineq, -np.inf, 0)
